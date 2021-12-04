27a285ee4e59d4a7b09fdc5384df50a2d394301f
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-12-03 21:02:02 -0800

    register middlewares on routes

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+const listsRouter = require('./lists/router');
+
+const router = Router();
+const api = '/api/v1';
+
+// middlewares
+const errorHandlerMiddleware = require('../middlewares/errorHandler');
+const customResponse = require('../middlewares/customResponse');
+
+router.use(customResponse);
+
+// app routes
+router.use(`${api}/lists`, listsRouter);
+// router.use(`${api}/users`, usersRouter);
+// router.use(`${api}/themes`, themesRouter);
+
+router.use(errorHandlerMiddleware);
+
+module.exports = router;
