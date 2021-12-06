a2000537f5f04d221620cf22f5d7edef43387df2
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-12-06 13:02:41 -0800

    refactor response middleware

--- middlewares/customResponse.js
+++ middlewares/customResponse.js
@@ -1,55 +1,15 @@
 const httpResponse = require('../common/httpResponse');
+const httpVerbs = require('../common/constants/httpVerbs');
+const capitalize = require('../common/utils/capitalize');
 
 module.exports = (req, res, next) => {
-  res.Ok = (payload) => {
-    const response = httpResponse.ok(payload);
-    res.status(response.statusCode).json(response);
-  };
-
-  res.Created = (payload) => {
-    const response = httpResponse.created(payload);
-    res.status(response.statusCode).json(response);
-  };
-
-  res.Accepted = () => {
-    const response = httpResponse.accepted();
-    res.status(response.statusCode).json(response);
-  };
-
-  res.BadRequest = (fields, message) => {
-    const response = httpResponse.badRequest(fields, message);
-    res.status(response.statusCode).json(response);
-  };
-
-  res.Unauthorized = (message) => {
-    const response = httpResponse.unauthorized(message);
-    res.status(response.statusCode).json(response);
-  };
-
-  res.Forbidden = (message) => {
-    const response = httpResponse.forbidden(message);
-    res.status(response.statusCode).json(response);
-  };
-
-  res.NotFound = (message) => {
-    const response = httpResponse.notFound(message);
-    res.status(response.statusCode).json(response);
-  };
-
-  res.Conflict = (message) => {
-    const response = httpResponse.conflict(message);
-    res.status(response.statusCode).json(response);
-  };
-
-  res.UnprocessableEntity = (fields, message) => {
-    const response = httpResponse.unprocessableEntity(fields, message);
-    res.status(response.statusCode).json(response);
-  };
-
-  res.InternalServerError = (message) => {
-    const response = httpResponse.internalServerError(payload);
-    res.status(response.statusCode).json(response);
-  };
+  // inject http method into express res object
+  httpVerbs.forEach((method) => {
+    res[capitalize(method)] = (x, y) => {
+      const response = httpResponse[method](x, y);
+      res.status(response.statusCode).json(response);
+    };
+  });
 
   next();
 };
