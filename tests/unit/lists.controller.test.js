65ef197e1e326077f38afc8cb105171d4de4f7ce
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-12-06 13:04:24 -0800

    add temporary lists controller test file

---
+++ tests/unit/lists.controller.test.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const getMockRes = require('../helpers/mockRes');
+const { getMockReq } = require('@jest-mock/express');
+const listsController = require('../../routes/lists/controller');
+const ListModel = require('../../models/List');
+
+jest.mock('../../models/List');
+
+const res = getMockRes();
+
+describe('Lists Controller GET', () => {
+  test('When valid params are provided, expect response to be success', async () => {
+    // Arange
+    const req = getMockReq({ query: {} });
+
+    // mocks
+    const lists = { docs: [] };
+    ListModel.paginate.mockResolvedValue(lists);
+
+    // Act
+    const httpResponse = await listsController.get(req, res);
+
+    // Assert
+    expect(httpResponse).toMatchObject({
+      statusCode: 200,
+      statusMessage: 'Ok',
+      payload: lists,
+      errorMessage: null,
+      validationErrors: [],
+    });
+  });
+
+  // TODO:
+  // 'When page param is invalid, expect validation errors'
+  // 'When pageSize param is invalid, expect response to have validation errors'
+  // 'When name param is invalid, expect response to have validation errors'
+});
