cac3e40eeb9ae3f7618c6e3d1378bff82900ddb8
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-12-14 21:40:22 -0800

    add Languages Model

---
+++ models/Language.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+
+const mongoosePaginate = require('mongoose-paginate-v2');
+const mongooseNormalize = require('@meanie/mongoose-to-json');
+
+const LanguageSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  createDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+LanguageSchema.plugin(mongoosePaginate);
+LanguageSchema.plugin(mongooseNormalize);
+
+module.exports = model('Language', LanguageSchema);
