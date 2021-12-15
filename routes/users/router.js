c3e931007217a4850d587efeb476f449445def4c
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-12-15 09:27:34 -0800

    add users router

--- routes/users/router.js
+++ routes/users/router.js
@@ -4,10 +4,11 @@ const usersController = require('./controller');
 const requireAuth = require('../../middlewares/requireAuth');
 
 const router = Router()
-  .post('/', requireAuth, usersController.post)
   .post('/login', usersController.login)
+  .put('/password', usersController.passwordReset)
+  .post('/', usersController.post)
   .get('/', requireAuth, usersController.get)
-  .patch('/:id', requireAuth, usersController.patch)
-  .delete(':id', requireAuth, usersController.delete);
+  .patch('/:userId', requireAuth, usersController.patch)
+  .delete('/:userId', requireAuth, usersController.delete);
 
 module.exports = router;
